005378d2c87cb6237e8105e3f0b8351d57c28342
taipv-angelhack <tai.pham@angelhack.com>
2025-11-01 23:44:06 +0700

    fix build

--- src/components/pages/HospitalLanding2.tsx
+++ src/components/pages/HospitalLanding2.tsx
@@ -8,7 +8,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { useState } from 'react';
 
 interface SpecializedUnit {
   name: string;
